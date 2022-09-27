49378f866af79971c25cfd245b6056747f445ddd
Nathan Clevenger <nateclev@gmail.com>
2022-09-27 13:38:54 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -20,10 +20,15 @@ export const api = {
 export default {
   fetch: async (req, env) => {
     const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, url, query } = await env.CTX.fetch(req).then(res => res.json())
+    
+    let file, ast, error = undefined
+    try {
+      file = await fetch('https:/' + pathname, req).then(res => res.text()).catch()
+      ast = file ? esprima.parseScript(file) : esprima.parseScript(pathSegment[0]))
+    } catch ({name,message}) {
+      error = {name,message}
+    }
 
-    let file = await fetch('https:/' + pathname, req).then(res => res.text()).catch()
-    const ast = file ? esprima.parseScript(file) : pathSegments.map(segment => esprima.parseScript(segment))
-
-    return new Response(JSON.stringify({ api, ast, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+    return new Response(JSON.stringify({ api, file, ast, error, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
   },
 }
